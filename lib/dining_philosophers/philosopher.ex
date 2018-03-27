1deb84cf2e6a8cd57fcc4a145cb9ff73ce050a6f
Nic McPhee <mcphee@morris.umn.edu>
2018-03-27 12:51:49 -0500

    Add fib calculations to philosophers
    This adds a version of "thinking" where the
    philosophers compute a given value of
    fib as part of their thinking. We might
    want to restructure this so that we
    return to eating as soon as that fib is
    calculated.

--- lib/dining_philosophers/philosopher.ex
+++ lib/dining_philosophers/philosopher.ex
@@ -2,9 +2,8 @@ defmodule Philosopher do
   def thinking(philosopher_number, leftChopstick, rightChopstick) do
     receive do
       {:eat, from} ->
-	
-	IO.puts("Philosopher #{philosopher_number} is now eating")
-	eating(philosopher_number, leftChopstick, rightChopstick)
+        IO.puts("Philosopher #{philosopher_number} is now eating")
+        eating(philosopher_number, leftChopstick, rightChopstick)
       {:finish} ->
         exit(:normal)
     end
@@ -14,11 +13,25 @@ defmodule Philosopher do
     receive do
       {:think, from} ->
         IO.puts("Philosopher #{philosopher_number} is now thinking")
-	send(leftChopstick, {:release, self()})
-	send(rightChopstick, {:release, self()})
+	      send(leftChopstick, {:release, self()})
+	      send(rightChopstick, {:release, self()})
+        thinking(philosopher_number, leftChopstick, rightChopstick)
+      # A philosopher can be asked to "think" by computing fib(n)
+      # for the specified value of n.
+      {:think, n, from} ->
+        IO.puts("Philosopher #{philosopher_number} is computing fib(#{n})")
+	      send(leftChopstick, {:release, self()})
+	      send(rightChopstick, {:release, self()})
+        result = fib(n)
+        IO.puts("Philosopher #{philosopher_number} found fib(#{n}) = #{result}")
+        send(from, {:ok, result})
         thinking(philosopher_number, leftChopstick, rightChopstick)
       {:finish} ->
         exit(:normal)
     end
   end
+
+  defp fib(0), do: 0
+  defp fib(1), do: 1
+  defp fib(n), do: fib(n-2) + fib(n-1) 
 end
